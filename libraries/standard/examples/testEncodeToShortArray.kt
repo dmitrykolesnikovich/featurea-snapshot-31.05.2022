c354d80d358daf8e87f52a345ae6a23c3d68a8bf
Dmitry Kolesnikovich <dmitrykolesnikovich@users.noreply.github.com>
2022-02-23 00:41:22 +0300

    Refactoring.

---
+++ libraries/standard/examples/testEncodeToShortArray.kt
@@ -0,0 +1,13 @@
+package featurea.examples
+
+import featurea.Encoding
+import featurea.encodeToByteArray
+import featurea.encodeToShortArray
+import java.nio.charset.Charset
+
+fun testEncodeToShortArray() {
+    println("191d49c180aca2e5".encodeToShortArray(count = 8).joinToString())
+    println("688df680e608608b".encodeToShortArray(count = 8).joinToString())
+    println(shortArrayOf(12601, 12644, 13369, 25393, 14384, 24931, 24882, 25909).encodeToByteArray(Encoding.BIG_ENDIAN).toString(Charset.forName("ISO-8859-1")))
+    println(shortArrayOf(13880, 14436, 26166, 14384, 25910, 12344, 13872, 14434).encodeToByteArray(Encoding.BIG_ENDIAN).toString(Charset.forName("ISO-8859-1")))
+}
