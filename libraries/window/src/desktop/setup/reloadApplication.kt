86942e6baf2e1d1a1c253d702dc5965664cbd7fe
Dmitry Kolesnikovich <dmitrykolesnikovich@users.noreply.github.com>
2022-05-07 14:28:29 +0300

    Refactoring.

--- libraries/window/src/desktop/setup/reloadApplication.kt
+++ libraries/window/src/desktop/setup/reloadApplication.kt
@@ -1,12 +1,12 @@
 package featurea.window
 
 import featurea.loader.clearCaches
-import featurea.reload
+import featurea.utils.reload
 import featurea.runtime.Component
 import featurea.runtime.Module
 import featurea.runtime.container
 
-fun Component.reloadWindow() {
+fun Component.reloadApplication() {
     val appModule: Module = container.modules["featurea.ApplicationModule"]
     container.clearCaches()
     container.reload(appModule)
