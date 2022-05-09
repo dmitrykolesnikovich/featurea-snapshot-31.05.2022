0bdb201a60049b028f161df1ca010b6eac526768
Dmitry Kolesnikovich <dmitrykolesnikovich@users.noreply.github.com>
2022-05-09 18:55:38 +0300

    Refactoring.

--- libraries/content/src/ResourceWriter.kt
+++ libraries/content/src/ResourceWriter.kt
@@ -2,6 +2,7 @@ package featurea.content
 
 import featurea.Bundle
 
+// todo migrate to Kotlin 1.6.20 to apply `context(ContainerContext)`
 interface ResourceWriter {
     suspend fun accept(resourceTag: ResourceTag, key: String, value: String, bundle: Bundle): Boolean = true
     suspend fun write(resourceTag: ResourceTag, key: String, value: String, bundle: Bundle)
